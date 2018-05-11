9a5dabda3f7b1c8c00ed1e636e0053dd93cdc639
andydevs <akanshul97@gmail.com>
2018-05-10 22:14:31 -0400

    Updated publicPath to change in production environment

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.join(__dirname, 'build'),
-        publicPath: '/'
+        publicPath: ( ENV === 'production' ? '/velocibox-clock-ui/' : '/' )
     },
     module: {
         loaders: [
